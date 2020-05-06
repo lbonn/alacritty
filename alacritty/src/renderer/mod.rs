ac0c8b6ca751981193bb7de9c175a14827945855
lbonn <bonnans.l@gmail.com>
2020-05-06 20:30:16 +0200

    Reduce Renderer's max batch size
    It's quite excessive

--- alacritty/src/renderer/mod.rs
+++ alacritty/src/renderer/mod.rs
@@ -531,7 +531,7 @@ impl Batch {
 }
 
 /// Maximum items to be drawn in a batch.
-const BATCH_MAX: usize = 0x1_0000;
+const BATCH_MAX: usize = 0x1000;
 const ATLAS_SIZE: i32 = 1024;
 
 impl QuadRenderer {
